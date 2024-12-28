e446a1992118ed724a0146c1fee94fd873b34107
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-12-28 13:01:56 +0100

    feat: delete create user from User controller

--- carry/apps/account/src/app/user/user.controller.ts
+++ carry/apps/account/src/app/user/user.controller.ts
@@ -1,16 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
-
-  @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
-  }
 
   @Get()
   getUsers() {
